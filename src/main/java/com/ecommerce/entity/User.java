d002454961097c4e4fffa34bf95b446d0250d9cf
haranadh03 <haranadhchowdary1436@gmail.com>
2026-07-18 22:28:23 +0530

    newfiles

--- src/main/java/com/ecommerce/entity/User.java
+++ src/main/java/com/ecommerce/entity/User.java
@@ -1,5 +1,117 @@
 package com.ecommerce.entity;
 
-public class User {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String firstName;
+	private String lastName;
+	@Column(unique= true, nullable = false)
+	private String email;
+	@Column(nullable=false)
+	private String password;
+	private String phone;
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	private boolean enabled;
+	private LocalDateTime createdAt;
+	private LocalDateTime updateAt;
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", phone=" + phone + ", role=" + role + ", enabled=" + enabled
+				+ ", createdAt=" + createdAt + ", updateAt=" + updateAt + "]";
+	}
+	public User(long id, String firstName, String lastName, String email, String password, String phone, Role role,
+			boolean enabled, LocalDateTime createdAt, LocalDateTime updateAt) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.role = role;
+		this.enabled = enabled;
+		this.createdAt = createdAt;
+		this.updateAt = updateAt;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
 }
